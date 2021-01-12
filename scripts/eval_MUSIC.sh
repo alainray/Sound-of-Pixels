0d62ed7aa77c05d5d349977c522f1d1aec81da6b
alainray <alainray@gmail.com>
2021-01-11 22:32:21 -0300

    Update eval_MUSIC.sh

--- scripts/eval_MUSIC.sh
+++ scripts/eval_MUSIC.sh
@@ -4,7 +4,7 @@ OPTS=""
 OPTS+="--mode eval "
 OPTS+="--id MUSIC-2mix-LogFreq-resnet18dilated-unet7-linear-frames3stride24-maxpool-binary-weightedLoss-channels32-epoch100-step40_80 "
 OPTS+="--list_val ../MUSIC_proc/val.csv "
-
+OPTS+="--list_train ../MUSIC_proc/train.csv "
 # Models
 OPTS+="--arch_sound unet7 "
 OPTS+="--arch_synthesizer linear "
